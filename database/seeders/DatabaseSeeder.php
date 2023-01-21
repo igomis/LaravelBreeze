1f96f1121f3ca651461adefb9260081f7a5887fb
Rubén Catalán <rucaju5@gmail.com>
2023-01-21 21:26:03 +0100

    [ADD] Seeder Gangas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
+use App\Models\Ganga;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -23,5 +24,6 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call(CategorySeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(GangaSeeder::class);
     }
 }
